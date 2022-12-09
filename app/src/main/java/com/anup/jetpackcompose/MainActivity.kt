3f6853923bdba74d00b551c771b2b4090fc7900d
Anup <hianup05@gmail.com>
2022-12-09 15:20:13 +0530

    Commit: ImageCard designing

--- app/src/main/java/com/anup/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/anup/jetpackcompose/MainActivity.kt
@@ -1,49 +1,81 @@
 package com.anup.jetpackcompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import com.anup.jetpackcompose.ui.theme.JetpackComposeTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier
-                .background(Color.Green)
-                .fillMaxHeight(.5f)
-                .fillMaxWidth()
-                .border(5.dp, Color.Blue)
-                .padding(top = 5.dp)
-                .border(15.dp, Color.Yellow)
-                .padding(10.dp)
-                .border(15.dp, Color.Red)
-                .padding(16.dp)) {
-                Text(text = "Hello", modifier = Modifier.clickable {
-                    Log.e("Clicked","hello")
-                })
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "World")
+            val painter = painterResource(id = R.drawable.shinchan)
+            val title = "Shinchan"
+            val description="Shinchan bhaiya ki yoooy oyoyoy!"
+            Box(modifier = Modifier
+                .fillMaxWidth(.5f)
+                .padding(16.dp)){
+                ImageCard(painter = painter, contentDescriptor = description, title = title)
             }
         }
     }
 }
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescriptor: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)){
+            Image(painter = painter,
+                contentDescription = contentDescriptor,
+            contentScale = ContentScale.Crop)
+
+            Box(modifier = Modifier.fillMaxSize()
+                .background(Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ),
+                    startY = 350f
+                )))
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            contentAlignment = Alignment.BottomStart){
+                Text(text = contentDescriptor, style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp
+                ))
+            }
+
+        }
+    }
+}
+
+
 
 
